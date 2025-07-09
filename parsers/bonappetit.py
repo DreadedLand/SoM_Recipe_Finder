02cb03b993596b40cdec8b6adba28f228124f7fc
DreadedLand <71191438+DreadedLand@users.noreply.github.com>
2025-07-09 16:32:18 -0500

    Update bonappetit.py
    Added logic to scrape site

--- parsers/bonappetit.py
+++ parsers/bonappetit.py
@@ -1,20 +1,44 @@
+import json
+
 from bs4 import BeautifulSoup
 
+# basically same logic as epicurious yipee
 def parse_bonappetit(response):
-    soup = BeautifulSoup(response.text, "html.parser")
+    try:
+        soup = BeautifulSoup(response.text, "html.parser")
 
-    title_tag = soup.find("h1", class_="post__title")
-    if not title_tag:
-        title_tag = soup.find("h1", {"data-testid": "ContentHeaderHed"})
-    title = title_tag.get_text(strip=True) if title_tag else None
+        title_tag = soup.find("h1", class_="post__title")
+        if not title_tag:
+            title_tag = soup.find("h1", {"data-testid": "ContentHeaderHed"})
+        title = title_tag.get_text(strip=True) if title_tag else None
 
-    ingredient_tags = soup.select("div.ingredient__text")
-    if not ingredient_tags:
-        ingredient_tags = soup.select("li[data-testid='IngredientListItem']")
+        contentScript = soup.find_all("script", type="application/ld+json")
 
-    ingredients = [tag.get_text(strip=True) for tag in ingredient_tags if tag.get_text(strip=True)]
+        for tag in contentScript:
+            try:
+                data = json.loads(tag.string)
 
-    return {
-        "title": title,
-        "ingredients": ingredients
-    }
+                if isinstance(data, list):
+                    for item in data:
+                        if item.get("@type") == "Recipe":
+                            data = item
+                            break
+
+                if data.get("@data") != "Recipe":
+                    continue
+
+                ingredients = data.get("recipeIngredient", [])
+                rating = str(data.get("AggregateRating", {}).get("RatingValue", "N/A"))
+
+                if ingredients:
+                    return {
+                        "title": title.strip(),
+                        "ingredients": [i.ingredients for i in ingredients],
+                        "rating": rating.strip()
+                    }
+            except Exception:
+                continue
+    except Exception:
+        pass
+
+    return None

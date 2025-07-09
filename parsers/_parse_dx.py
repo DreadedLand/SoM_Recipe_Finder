fc54d0a3e6fec09ec343e0c1fedf3d6075fccfb2
DreadedLand <71191438+DreadedLand@users.noreply.github.com>
2025-07-09 16:29:58 -0500

    Update _parse_dx.py
    Added support to easily upscale recipes.

--- parsers/_parse_dx.py
+++ parsers/_parse_dx.py
@@ -1,5 +1,3 @@
-from urllib.parse import urlparse
-
 from .allrecipes import parse_allrecipes
 from .bonappetit import parse_bonappetit
 from .epicurious import parse_epicurious
@@ -7,15 +5,15 @@ from .foodcom import parse_foodcom
 from .foodnetwork import parse_foodnetwork
 
 DOMAINS = {
-    "allrecipes.com": parse_allrecipes,
-    "bonappetit.com": parse_bonappetit,
-    "epicurious.com": parse_epicurious,
-    "food.com": parse_foodcom,
-    "foodnetwork.com": parse_foodnetwork
+    "allrecipes.com": [parse_allrecipes, r"/recipe/\d+/"],
+    "bonappetit.com": [parse_bonappetit, r"/recipe/"],
+    "epicurious.com": [parse_epicurious, r"/recipes/food/views/"],
+    # "food.com": [parse_foodcom,],
+    "foodnetwork.com": [parse_foodnetwork, r"-recipe-\d+/"]
 }
 
 def get_recipe_data(response):
-    for domain, func in DOMAINS.items():
+    for domain, (func, _) in DOMAINS.items():
         if domain in response.url:
-            return func(response)
+            return func[0](response)
     return None
